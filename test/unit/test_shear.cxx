70ea2ed36f5ac0b8388dcc15d7c1210d2e39332a
Daniel Stotts <dstodev@gmail.com>
2022-02-03 13:19:22 -0800

    Tests are slightly more sane.
    Moves the instantiation of 'expected' variables above shear() so that we guarantee expected values are not affected by it.

--- test/unit/test_shear.cxx
+++ test/unit/test_shear.cxx
@@ -9,9 +9,9 @@ TEST(Shear, zero_magnitudes)
 {
 	Vector2d point {1.0f, 2.0f};
 
-	shear(point, 0, 0);
-
 	const Vector2d expected {1.0f, 2.0f};
+
+	shear(point, 0, 0);
 
 	ASSERT_EQ(expected, point);
 }
@@ -20,12 +20,12 @@ TEST(Shear, x_magnitude_2)
 {
 	Vector2d point {1.0f, 2.0f};
 
-	shear(point, 2, 0);
-
 	// x' = x + shear[x] * y
 	//    = 1 + 2        * 2
 	//    = 5
 	const Vector2d expected {5.0f, point[1]};
+
+	shear(point, 2, 0);
 
 	ASSERT_EQ(expected, point);
 }
@@ -34,12 +34,12 @@ TEST(Shear, y_magnitude_2)
 {
 	Vector2d point {1.0f, 2.0f};
 
-	shear(point, 0, 2);
-
 	// y' = y + shear[y] * x
 	//    = 2 + 2        * 1
 	//    = 4
 	const Vector2d expected {point[0], 4.0f};
+
+	shear(point, 0, 2);
 
 	ASSERT_EQ(expected, point);
 }
@@ -48,11 +48,11 @@ TEST(Shear, both_magnitudes_2)
 {
 	Vector2d point {1.0f, 2.0f};
 
-	shear(point, 2, 2);
-
 	// x' = 5
 	// y' = 4
 	const Vector2d expected {5.0f, 4.0f};
+
+	shear(point, 2, 2);
 
 	ASSERT_EQ(expected, point);
 }
