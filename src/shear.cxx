956bac2fc02a6237f57ee98d7020f0c9649bec31
Daniel Stotts <dstodev@gmail.com>
2022-02-06 18:10:27 -0800

    Improves shear performance

--- src/shear.cxx
+++ src/shear.cxx
@@ -2,9 +2,8 @@
 
 void my::shear(my::point_t& point, float x_magnitude, float y_magnitude)
 {
-	static Eigen::Matrix2f shear;
+	static Eigen::Matrix2f shear {{1, 0}, {0, 1}};
 
-	shear.setIdentity();
 	shear(0, 1) = x_magnitude;
 	shear(1, 0) = y_magnitude;
 
