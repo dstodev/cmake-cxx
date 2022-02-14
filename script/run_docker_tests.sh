562ceb50d213c11c91cfc2b25e569a89ad872a3b
Daniel Stotts <dstodev@gmail.com>
2022-02-13 18:42:24 -0800

    Script pathing improvements

--- script/run_docker_tests.sh
+++ script/run_docker_tests.sh
@@ -6,5 +6,5 @@ docker_dir="$source_dir/docker"
 
 image_name='build:ubuntu2004'
 
-docker build --file "$docker_dir/ubuntu2004.dockerfile" $docker_dir --tag $image_name
-docker run --rm --volume "$source_dir:/temp:ro" $image_name '/temp/script/run_tests.sh' '/build'
+docker build --file "$docker_dir/ubuntu2004.dockerfile" "$docker_dir" --tag "$image_name"
+docker run --rm --volume "$source_dir:/temp:ro" "$image_name" '/temp/script/run_tests.sh' '/build'
--- script/run_tests.sh
+++ script/run_tests.sh
@@ -3,13 +3,13 @@
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 source_dir="$script_dir/.."
 
-$script_dir/fetch_dependencies.sh $1 || exit $?
-
 if [ -z "$1" ]; then
 	build_dir="$source_dir/build"
 else
 	build_dir="$1"
 fi
 
-cmake -B $build_dir -S $source_dir
-cmake --build $build_dir --target all test
+"$script_dir/fetch_dependencies.sh" "$build_dir" || exit $?
+
+cmake -B "$build_dir" -S "$source_dir"
+cmake --build "$build_dir" --target all test
