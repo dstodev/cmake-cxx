3b3972db31fda57cdb42486cc53d66eb2649182c
Daniel Stotts <dstodev@gmail.com>
2022-11-13 18:14:16 -0800

    Updates tests

--- lib/cmake/AddLocalAllTargetConfig.cmake
+++ lib/cmake/AddLocalAllTargetConfig.cmake
@@ -1,4 +1,11 @@
-function(add_local_all_target prefix)
+function(add_local_all_target target_name)
+	list(APPEND argv "${target_name}")
+
 	get_directory_property(targets BUILDSYSTEM_TARGETS)
-	add_custom_target(${prefix} ${targets})
+
+	foreach(target IN LISTS targets)
+		list(APPEND argv "COMMAND;${target}")
+	endforeach()
+
+	add_custom_target(${argv})
 endfunction()
