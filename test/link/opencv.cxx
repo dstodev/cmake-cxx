b91658a4e6a76a3e0bb2ac48798c53a28d46afcf
Daniel Stotts <dstodev@gmail.com>
2022-12-18 13:36:37 -0800

    Adds to opencv sanity check

--- test/link/opencv.cxx
+++ test/link/opencv.cxx
@@ -9,6 +9,7 @@ using namespace cv;
 TEST(Link, opencv)
 {
 	Mat o;
+	(void) o;
 }
 
 // Include "sanity check" tests in the Link suite
@@ -18,41 +19,46 @@ TEST(Link, opencv)
 
 /* Interpret as flat (1-dimensional) array of RGB values where every
  * 3 values (R (0-255), G (0-255), B (0-255)) represent one pixel.
- * This variable represents 6 pixels, each with color:
+ * This variable represents 8 RGB pixels:
  *
- *   R G B   R red   (255, 0,   0)    C cyan    (0,   255, 255)
- *   C M Y   G green (0,   255, 0)    M magenta (255, 0,   255)
- *           B blue  (0,   0,   255)  Y yellow  (255, 255, 0)
+ *   R G B W  R red   (255, 0,   0)    C cyan    (0,   255, 255)
+ *   C M Y K  G green (0,   255, 0)    M magenta (255, 0,   255)
+ *            B blue  (0,   0,   255)  Y yellow  (255, 255, 0)
+ *            W white (255, 255, 255)  K black   (0,   0,   0)
  */
 // clang-format off
-static constexpr unsigned char RGBCMY[] {
+static constexpr unsigned char RGBWCMYK[] {
 //  R    G    B
     255, 0,   0,    // R
     0,   255, 0,    // G
     0,   0,   255,  // B
+    255, 255, 255,  // W
     0,   255, 255,  // C
     255, 0,   255,  // M
-    255, 255, 0     // Y
+    255, 255, 0,    // Y
+    0,   0,   0     // K
 };
 // clang-format on
 
 TEST(Sanity, opencv_load_rgb)
 {
-	Mat pixels(2, 3, CV_8UC3, (void*) RGBCMY);
-	Mat converted(pixels.size(), pixels.type());
+	Mat pixels(2, 4, CV_8UC3, (void*) RGBWCMYK);
 
 	// Assert pixels were loaded in exact RGB order before converting
 	// color space
-	ASSERT_EQ(Vec3b({255, 0, 0}), pixels.at<Vec3b>(0, 0));  // R
-	ASSERT_EQ(Vec3b({0, 255, 0}), pixels.at<Vec3b>(0, 1));  // G
-	ASSERT_EQ(Vec3b({0, 0, 255}), pixels.at<Vec3b>(0, 2));  // B
-	ASSERT_EQ(Vec3b({0, 255, 255}), pixels.at<Vec3b>(1, 0));  // C
-	ASSERT_EQ(Vec3b({255, 0, 255}), pixels.at<Vec3b>(1, 1));  // M
-	ASSERT_EQ(Vec3b({255, 255, 0}), pixels.at<Vec3b>(1, 2));  // Y
+	/* R */ EXPECT_EQ(Vec3b({255, 0, 0}), pixels.at<Vec3b>(0, 0));
+	/* G */ EXPECT_EQ(Vec3b({0, 255, 0}), pixels.at<Vec3b>(0, 1));
+	/* B */ EXPECT_EQ(Vec3b({0, 0, 255}), pixels.at<Vec3b>(0, 2));
+	/* W */ EXPECT_EQ(Vec3b({255, 255, 255}), pixels.at<Vec3b>(0, 3));
+	/* C */ EXPECT_EQ(Vec3b({0, 255, 255}), pixels.at<Vec3b>(1, 0));
+	/* M */ EXPECT_EQ(Vec3b({255, 0, 255}), pixels.at<Vec3b>(1, 1));
+	/* Y */ EXPECT_EQ(Vec3b({255, 255, 0}), pixels.at<Vec3b>(1, 2));
+	/* K */ EXPECT_EQ(Vec3b({0, 0, 0}), pixels.at<Vec3b>(1, 3));
 
 	// OpenCV expects BGR color space
+	Mat converted(pixels.size(), pixels.type());
 	cvtColor(pixels, converted, COLOR_RGB2BGR);
 
-	// Save visual-inspection test image
-	imwrite("opencv-sanity-check-RGBCMY.png", converted);
+	// Save visual-inspection-test image
+	imwrite("opencv-sanity-check-rgbwcmyk.png", converted);
 }
