0125c18f9f71a45fe51b7082f0868dc9ee230511
Daniel Stotts <dstodev@gmail.com>
2022-12-20 17:46:19 -0800

    MY_ASSERT_SAME_TYPE has better error messaging

--- test/unit/assertions.hxx
+++ test/unit/assertions.hxx
@@ -8,7 +8,7 @@
 #define MY_ASSERT_SAME_TYPE(expected_type, actual_type) \
 	do { /* This is the 'do {} while (0)' macro idiom */ \
 		auto predicate = std::is_same_v<expected_type, actual_type>; \
-		ASSERT_TRUE(predicate); \
+		ASSERT_TRUE(predicate) << "  Reason: " << #expected_type << " != " << #actual_type; \
 	} while (0)
 
 #endif  // ASSERTIONS_HXX
