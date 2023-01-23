fe0986239fa63508cc5bbc2c1eb19fc326e53e1b
Daniel Stotts <dstodev@gmail.com>
2023-01-22 18:57:54 -0800

    Cobwebs

--- lib/cmake/SetupBuildModeConfig.cmake
+++ lib/cmake/SetupBuildModeConfig.cmake
@@ -12,18 +12,18 @@ function(setup_build_mode default_mode)
 	if (is_multi_config)
 		_multi_config()
 	else()
-		string_capitalize(${default_mode} default_mode)
 		_single_config("${default_mode}")
 	endif()
 endfunction()
 
 function(_multi_config)
 	# https://cmake.org/cmake/help/latest/variable/CMAKE_CONFIGURATION_TYPES.html#variable:CMAKE_CONFIGURATION_TYPES
-	# TODO
+	# Nothing to do; configure for all modes!
 endfunction()
 
 function(_single_config default_mode)
 	if (NOT CMAKE_BUILD_TYPE)  # if not set or if empty string
+		string_capitalize(${default_mode} default_mode)
 		message(WARNING "CMAKE_BUILD_TYPE unset or evaluated FALSE. Forcing CMAKE_BUILD_TYPE to: ${default_mode}")
 		# Variable may already exist in cache (as e.g. empty string), so FORCE
 		set(CMAKE_BUILD_TYPE "${default_mode}" CACHE STRING "Build mode" FORCE)
