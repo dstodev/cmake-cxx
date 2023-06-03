9409be01484af5a99cb0fffa849e14539099fc10
Daniel <dstodev@gmail.com>
2023-06-03 13:01:06 -0700

    Organizes install directives into export.cmake

---
+++ export.cmake
@@ -0,0 +1,38 @@
+set(export_name "project")
+
+# Package the application
+install(TARGETS
+	# Project targets
+	project
+	main
+
+	# Dependency targets
+	eigen
+	SDL2
+
+	EXPORT ${export_name}
+)
+install(DIRECTORY "${eigen_SOURCE_DIR}/Eigen" DESTINATION include)
+install(DIRECTORY "${SDL2_SOURCE_DIR}/include/" DESTINATION include/SDL2)
+
+# Redistribute system runtimes
+set(CMAKE_INSTALL_DEBUG_LIBRARIES TRUE)
+set(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP TRUE)
+include(InstallRequiredSystemLibraries)
+
+set(release_runtimes ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+list(FILTER release_runtimes EXCLUDE REGEX "Debug")
+
+set(debug_runtimes ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+list(FILTER debug_runtimes INCLUDE REGEX "Debug")
+
+install(PROGRAMS ${release_runtimes} DESTINATION bin CONFIGURATIONS Release)
+install(PROGRAMS ${debug_runtimes} DESTINATION bin CONFIGURATIONS Debug)
+
+log_vars(release_runtimes debug_runtimes SPLIT_LISTS MODE DEBUG)
+
+# Export CMake targets
+install(EXPORT ${export_name}
+	DESTINATION cmake
+	NAMESPACE project::
+)
