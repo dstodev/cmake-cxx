a6a6dc15e4d3c1f2d1f4b64941e6caed3f3ca145
dstodev <dstodev@gmail.com>
2023-06-25 17:37:29 -0700

    inline -> constexpr

--- src/log.hxx
+++ src/log.hxx
@@ -6,66 +6,66 @@
 namespace project::log {
 
 template <typename... Args>
-inline void error(char const* format, Args... args)
+void constexpr error(char const* format, Args... args)
 {
 #if ENABLE_LOGGING
 	SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 #endif
 }
 
-inline void error(char const* message)
+void constexpr error(char const* message)
 {
 	error("%s", message);
 }
 
 template <typename... Args>
-inline void warn(char const* format, Args... args)
+void constexpr warn(char const* format, Args... args)
 {
 #if ENABLE_LOGGING
 	SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 #endif
 }
 
-inline void warn(char const* message)
+void constexpr warn(char const* message)
 {
 	warn("%s", message);
 }
 
 template <typename... Args>
-inline void info(char const* format, Args... args)
+void constexpr info(char const* format, Args... args)
 {
 #if ENABLE_LOGGING
 	SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 #endif
 }
 
-inline void info(char const* message)
+void constexpr info(char const* message)
 {
 	info("%s", message);
 }
 
 template <typename... Args>
-inline void debug(char const* format, Args... args)
+void constexpr debug(char const* format, Args... args)
 {
 #if ENABLE_LOGGING
 	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 #endif
 }
 
-inline void debug(char const* message)
+void constexpr debug(char const* message)
 {
 	debug("%s", message);
 }
 
 template <typename... Args>
-inline void trace(char const* format, Args... args)
+void constexpr trace(char const* format, Args... args)
 {
 #if ENABLE_LOGGING
 	SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 #endif
 }
 
-inline void trace(char const* message)
+void constexpr trace(char const* message)
 {
 	trace("%s", message);
 }
