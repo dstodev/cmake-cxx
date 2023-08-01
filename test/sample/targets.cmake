283e4bc0327f8f2cea0adc6c6553e18aa9115e90
dstodev <dstodev@gmail.com>
2023-07-31 22:20:16 -0700

    Add package dependency explicitly

--- test/sample/targets.cmake
+++ test/sample/targets.cmake
@@ -2,8 +2,6 @@ add_custom_target(sample-app
 	COMMAND ${CMAKE_COMMAND} -S "${CMAKE_CURRENT_LIST_DIR}" -B "${PROJECT_BINARY_DIR}/sample" -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 	COMMAND ${CMAKE_COMMAND} --build "${PROJECT_BINARY_DIR}/sample" --parallel --config ${CMAKE_BUILD_TYPE} --target run
 )
-add_dependencies(sample-app
-	build-package
-)
+add_dependencies(sample-app build-package)
 
 set(SAMPLE_MAIN "${CMAKE_CURRENT_LIST_DIR}/main.cxx" PARENT_SCOPE)
