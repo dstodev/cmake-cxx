c29a42111e817878c649893d1d4f55f1aa81687f
dstodev <dstodev@gmail.com>
2023-08-11 22:57:08 -0700

    Reference single threaded counting benchmarks

--- test/performance/thread-pool-t.cxx
+++ test/performance/thread-pool-t.cxx
@@ -4,7 +4,52 @@
 
 using namespace project;
 
-static void run_threads(benchmark::State& state)
+static void count_single_threaded(benchmark::State& state)
+{
+	int const num_tasks = 4000;
+
+	for (auto _ : state) {
+		int count = 0;
+
+		for (int i = 0; i < num_tasks; ++i) {  // loop may be optimized away
+			count += (i + 1);
+		}
+
+		(void) count;
+	}
+}
+
+static void count_single_threaded_volatile(benchmark::State& state)
+{
+	int const num_tasks = 4000;
+
+	for (auto _ : state) {
+		volatile int count = 0;  // volatile prevents compiler from optimizing away the loop
+
+		for (int i = 0; i < num_tasks; ++i) {
+			count += (i + 1);
+		}
+
+		(void) count;
+	}
+}
+
+static void count_single_threaded_atomic(benchmark::State& state)
+{
+	int const num_tasks = 4000;
+
+	for (auto _ : state) {
+		volatile std::atomic_int count = 0;
+
+		for (int i = 0; i < num_tasks; ++i) {
+			count += (i + 1);
+		}
+
+		(void) count;
+	}
+}
+
+static void count_multithreaded(benchmark::State& state)
 {
 	int const num_threads = 40;
 	int const num_tasks = num_threads * 100;
@@ -21,4 +66,7 @@ static void run_threads(benchmark::State& state)
 	}
 }
 
-BENCHMARK(run_threads)->Iterations(1000)->Unit(benchmark::kMillisecond);
+BENCHMARK(count_single_threaded);
+BENCHMARK(count_single_threaded_volatile);
+BENCHMARK(count_single_threaded_atomic);
+BENCHMARK(count_multithreaded)->Iterations(1000)->Unit(benchmark::kMillisecond);
