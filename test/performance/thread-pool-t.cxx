c25694f6ca65db8aa87bf7cb0c52694b87d7974d
dstodev <dstodev@gmail.com>
2023-08-15 19:16:01 -0700

    Adds performance test

--- test/performance/thread-pool-t.cxx
+++ test/performance/thread-pool-t.cxx
@@ -65,6 +65,24 @@ static void count_multithreaded(benchmark::State& state)
 	}
 }
 
+static void count_multithreaded_initialized(benchmark::State& state)
+{
+	int const num_tasks = 1000;
+
+	thread_pool_t pool(state.range(0));
+
+	for (auto _ : state) {
+		volatile std::atomic_int count = 0;
+
+		for (int i = 0; i < num_tasks; ++i) {
+			pool.add_task([&count, i]() { count += (i + 1); });
+		}
+
+		pool.wait();
+		(void) count;
+	}
+}
+
 // clang-format off
 
 BENCHMARK(count_single_threaded);
@@ -73,7 +91,13 @@ BENCHMARK(count_single_threaded_atomic);
 
 BENCHMARK(count_multithreaded)
     ->RangeMultiplier(2)
-    ->Range(2, 2 << 4)
+    ->Range(1, 2 << 4)
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(1000);
+
+BENCHMARK(count_multithreaded_initialized)
+    ->RangeMultiplier(2)
+    ->Range(1, 2 << 4)
     ->Unit(benchmark::kMillisecond)
     ->Iterations(1000);
 
