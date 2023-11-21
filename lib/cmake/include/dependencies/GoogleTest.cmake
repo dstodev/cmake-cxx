8c0bb47ca07bc5168d44a471e3ac599a62e15fc4
dstodev <dstodev@gmail.com>
2023-11-21 00:29:31 -0800

    Update version & add note improving configuration time

--- lib/cmake/include/dependencies/GoogleTest.cmake
+++ lib/cmake/include/dependencies/GoogleTest.cmake
@@ -1,8 +1,14 @@
 # https://github.com/google/googletest
 
+#[[
+	If GoogleTest is taking a long time to add, it may be because it's trying
+	to find a Python interpreter. After I installed Python, GoogleTest
+	configuration time improved from around 7 seconds to under 1 second.
+]]
+
 CPMAddPackage(NAME googletest
 	GITHUB_REPOSITORY google/googletest
-	VERSION 1.13.0
+	VERSION 1.14.0
 	OPTIONS "BUILD_GMOCK OFF" "INSTALL_GTEST OFF" "gtest_force_shared_crt ON"
 	EXCLUDE_FROM_ALL TRUE
 	SYSTEM TRUE
