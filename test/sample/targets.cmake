9022eb09e5f739133b3cc56c9a53b06fca1f65b0
dstodev <dstodev@gmail.com>
2023-12-08 23:47:13 -0800

    style

--- test/sample/targets.cmake
+++ test/sample/targets.cmake
@@ -4,9 +4,8 @@ set(binary_dir "${PROJECT_BINARY_DIR}/sample")
 set(target_name "app-sample")
 
 add_custom_target(${target_name}
-	# When using multi-config generators, allow empty CMAKE_BUILD_TYPE (do not pass $<CONFIG> to CMAKE_BUILD_TYPE)
 	COMMAND ${CMAKE_COMMAND} -S "${source_dir}" -B "${binary_dir}" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
-	COMMAND ${CMAKE_COMMAND} --build "${binary_dir}" --config $<CONFIG> --target run --parallel
+	COMMAND ${CMAKE_COMMAND} --build "${binary_dir}" --config $<CONFIG> --parallel --target run
 )
 add_dependencies(${target_name} build-package)
 
