5fc3a17dcbee9888b2dbf67fbdb7702ec082707e
dstodev <dstodev@gmail.com>
2024-02-24 19:34:42 -0800

    Split cmd

--- sample/targets.cmake
+++ sample/targets.cmake
@@ -8,7 +8,7 @@ set(binary_dir "${PROJECT_BINARY_DIR}/sample")
 set(target_name "app-sample")
 
 add_custom_target(${target_name}
-	COMMAND ${CMAKE_COMMAND} -S "${source_dir}" -B "${binary_dir}" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
+	COMMAND ${CMAKE_COMMAND} -S "${source_dir}" -B "${binary_dir}" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"  # --log-level=DEBUG
 	COMMAND ${CMAKE_COMMAND} --build "${binary_dir}" --config $<CONFIG> --parallel --target run
 )
 add_dependencies(${target_name} build-package)
