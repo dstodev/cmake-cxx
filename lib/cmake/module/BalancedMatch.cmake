3ad837b4208b962de2ecc9c4fc3c0fc89280c34d
dstodev <dstodev@gmail.com>
2024-03-24 19:32:19 -0700

    add BalancedMatch

---
+++ lib/cmake/module/BalancedMatch.cmake
@@ -0,0 +1,166 @@
+include_guard()
+
+include(FindSubstrings)
+
+#[[
+	balanced_match(out_var left_delimiter right_delimiter in_str)
+
+	Matches all text within the outermost pair of balanced delimiters
+	in a string.
+
+	Stores in out_var the substring between the first occurrence of the
+	left delimiter and the paired occurrence of the right delimiter.
+
+	If no balanced pair is found, out_var is undefined.
+]]
+function(balanced_match out_var left_delimiter right_delimiter in_str)
+	string(LENGTH "${left_delimiter}" left_len)
+	string(LENGTH "${right_delimiter}" right_len)
+	string(LENGTH "${in_str}" in_len)
+
+	find_substrings(lefts "${left_delimiter}" "${in_str}")
+	find_substrings(rights "${right_delimiter}" "${in_str}")
+
+	if(NOT lefts OR NOT rights)
+		return()
+	endif()
+
+	# Remove right delimiters that appear before the first left delimiter
+	list(GET lefts 0 left)
+	foreach(right IN LISTS rights)
+		if(right LESS left)
+			list(REMOVE_ITEM rights ${right})
+		endif()
+	endforeach()
+
+	# Remove left delimiters that appear after the last right delimiter
+	list(GET rights -1 right)
+	foreach(left IN LISTS lefts)
+		if(left GREATER right)
+			list(REMOVE_ITEM lefts ${left})
+		endif()
+	endforeach()
+
+	list(LENGTH lefts num_lefts)
+	list(LENGTH rights num_rights)
+
+	if(NOT num_lefts EQUAL num_rights)
+		return()
+	endif()
+
+	set(left_depth 0)
+	set(left_pos -1)
+	set(right_pos -1)
+
+	while(left_depth GREATER -1)
+		# Delimiter positions are in order of appearance from left to right
+		list(GET lefts ${left_depth} left)
+
+		if(left_pos EQUAL -1)
+			# Latch first left delimiter position
+			set(left_pos ${left})
+		endif()
+
+		# Next right delimiter
+		list(GET rights 0 right)
+
+		# Look ahead to see if there is a closer left delimiter
+		math(EXPR next_depth "${left_depth} + 1")
+		list(LENGTH lefts num_lefts)
+
+		if(num_lefts GREATER next_depth)
+			list(GET lefts ${next_depth} next_left)
+
+			if(next_left LESS right)
+				# Skip the current left delimiter
+				math(EXPR left_depth "${left_depth} + 1")
+				continue()
+			endif()
+		endif()
+
+		# Now we know the right delimiter closes the current left delimiter
+		list(REMOVE_AT lefts ${left_depth})
+		math(EXPR left_depth "${left_depth} - 1")
+		list(POP_FRONT rights right)
+		set(right_pos ${right})
+	endwhile()
+
+	math(EXPR left_pos "${left_pos} + ${left_len}")
+	math(EXPR length "${right_pos} - ${left_pos}")
+
+	string(SUBSTRING "${in_str}" ${left_pos} ${length} out)
+	set(${out_var} "${out}" PARENT_SCOPE)
+endfunction()
+
+include(Expect)
+expect_test_preamble()
+
+function(test_balanced_match_simple)
+	set(input "some(thing)else")
+	balanced_match(out "(" ")" "${input}")
+	expect(DEFINED out)
+	expect("${out}" STREQUAL "thing")
+endfunction()
+test_balanced_match_simple()
+
+function(test_balanced_match_nested)
+	set(input "some(t(hi)ng)else")
+	balanced_match(out "(" ")" "${input}")
+	expect(DEFINED out)
+	expect("${out}" STREQUAL "t(hi)ng")
+endfunction()
+test_balanced_match_nested()
+
+function(test_balanced_match_triple_nested)
+	set(input "some((t(hi)ng))else")
+	balanced_match(out "(" ")" "${input}")
+	expect(DEFINED out)
+	expect("${out}" STREQUAL "(t(hi)ng)")
+endfunction()
+test_balanced_match_triple_nested()
+
+function(test_balanced_match_two_nested)
+	set(input "some(t(hi)(ng))else")
+	balanced_match(out "(" ")" "${input}")
+	expect(DEFINED out)
+	expect("${out}" STREQUAL "t(hi)(ng)")
+endfunction()
+test_balanced_match_two_nested()
+
+function(test_balanced_match_no_match)
+	set(input "some(thingelse")
+	balanced_match(out "(" ")" "${input}")
+	expect(NOT DEFINED out)
+
+	set(input "something)else")
+	balanced_match(out "(" ")" "${input}")
+	expect(NOT DEFINED out)
+endfunction()
+test_balanced_match_no_match()
+
+function(test_balanced_match_no_pair)
+	set(input "some(t(hing)else")
+	balanced_match(out "(" ")" "${input}")
+	expect(NOT DEFINED out)
+
+	set(input "some(thi)ng)else")
+	balanced_match(out "(" ")" "${input}")
+	expect(NOT DEFINED out)
+endfunction()
+test_balanced_match_no_pair()
+
+function(test_balanced_match_first)
+	set(input "some(thing)else(something)else")
+	balanced_match(out "(" ")" "${input}")
+	expect(DEFINED out)
+	expect("${out}" STREQUAL "thing")
+endfunction()
+test_balanced_match_first()
+
+function(test_balanced_match_wide_delimiters)
+	set(input "so(me((thing))el)se")
+	balanced_match(out "((" "))" "${input}")
+	expect(DEFINED out)
+	expect("${out}" STREQUAL "thing")
+endfunction()
+test_balanced_match_wide_delimiters()
--- lib/cmake/module/FindSubstrings.cmake
+++ lib/cmake/module/FindSubstrings.cmake
@@ -7,9 +7,6 @@ include_guard()
 
 	Parameters
 	----------
-	out_count :
-		Name of variable to store number of substring occurrences.
-
 	out_pos_list :
 		Name of variable to store list of occurrence positions.
 
@@ -19,10 +16,9 @@ include_guard()
 	in_string :
 		String to search in.
 ]]
-function(find_substrings out_count out_pos_list substring in_string)
+function(find_substrings out_pos_list substring in_string)
 	string(LENGTH "${substring}" substr_len)
 
-	set(count 0)
 	set(offset 0)
 
 	while(TRUE)
@@ -33,12 +29,10 @@ function(find_substrings out_count out_pos_list substring in_string)
 		math(EXPR offset "${offset} + ${pos}")
 		list(APPEND pos_list ${offset})
 		math(EXPR offset "${offset} + ${substr_len}")
-		math(EXPR count "${count} + 1")
 		math(EXPR pos "${pos} + ${substr_len}")
 		string(SUBSTRING "${in_string}" "${pos}" -1 in_string)
 	endwhile()
 
-	set(${out_count} ${count} PARENT_SCOPE)
 	set(${out_pos_list} "${pos_list}" PARENT_SCOPE)
 endfunction()
 
@@ -47,10 +41,8 @@ expect_test_preamble()
 
 function(test_find_substrings)
 	set(input "<>one<>two<><>three<><><>")
-	find_substrings(count pos_list "<>" "${input}")
-	expect(DEFINED count)
+	find_substrings(pos_list "<>" "${input}")
 	expect(DEFINED pos_list)
-	expect(${count} EQUAL 7)
 	list(LENGTH pos_list pos_list_len)
 	expect(${pos_list_len} EQUAL 7)
 	expect("${pos_list}" STREQUAL "0;5;10;12;19;21;23")
@@ -59,10 +51,8 @@ test_find_substrings()
 
 function(test_find_substrings_none)
 	set(input "<>one<>two<><>three<><><>")
-	find_substrings(count pos_list "()" "${input}")
-	expect(DEFINED count)
+	find_substrings(pos_list "()" "${input}")
 	expect(DEFINED pos_list)
-	expect(${count} EQUAL 0)
 	list(LENGTH pos_list pos_list_len)
 	expect(${pos_list_len} EQUAL 0)
 	expect("${pos_list}" STREQUAL "")
