774f6bed4dd6fac4aa590921d8731bb5aa433c87
Daniel Stotts <dstodev@gmail.com>
2024-03-31 22:57:06 -0700

    Improve Docker build script (bash)

--- script/run-docker-tests.bash
+++ script/run-docker-tests.bash
@@ -6,29 +6,59 @@ source_dir="$script_dir/.."
 docker_dir="$source_dir/docker"
 
 compose=(docker compose --file="$docker_dir/compose.yml")
+services=(
+  'build'
+  'build-clang'
+)
 
-services=$("${compose[@]}" config --services --quiet)
 runners=()
 
-for service in $services; do
-  echo "Starting service: $service..."
+echo
+
+for service in "${services[@]}"; do
+  echo "Starting service: $service"
   "${compose[@]}" build "$service"
   runners+=("$("${compose[@]}" run --detach "$service")")
+  echo
 done
 
 codes=""
 
-for runner in "${runners[@]}"; do
-  printf -- 'Waiting for %.8s... ' "$runner"
-  code=$(docker wait "$runner")
-  printf -- 'done.\n'
+# Calculate longest service name length (width)
+service_width=0
+for service in "${services[@]}"; do
+  width_candidate="${#service}"
 
-  printf -- "----------------------------------------\n"
-  docker logs "$runner"
-  printf -- "----------------------------------------\n"
+  if [ "$width_candidate" -gt "$service_width" ]; then
+    service_width="$width_candidate"
+  fi
+done
 
-  docker rm "$runner" >/dev/null
+log_paths=()
+
+for i in "${!runners[@]}"; do
+  run_id="${runners[i]}"
+  run_service="${services[i]}"
+  id_tag="$(printf -- '%.12s' "$run_id")"
+  printf -- "Waiting for: %-${service_width}s (%s...) " "$run_service" "$id_tag"
+  code="$(docker wait "$run_id")"
+
+  log_file="$run_service.log"
+  log_path="$script_dir/$log_file"
+  docker logs "$run_id" >"$log_path"
+  printf -- 'done. Log recorded: %s\n' "$log_file"
+  log_paths+=("$log_path")
+
+  docker rm "$run_id" >/dev/null
   codes+="$code"
+done
+
+echo
+
+for log_path in "${log_paths[@]}"; do
+  echo "Contents of log: $log_path"
+  cat -n "$log_path"
+  echo
 done
 
 # Look for non-zero exit codes. If any are found, exit with a non-zero code.
