8fbf778dd4bee6b924925e2a59213a1d2b051134
dstodev <dstodev@gmail.com>
2024-04-06 17:51:32 -0700

    Log policies while setting them

--- lib/cmake/module/SetPolicy.cmake
+++ lib/cmake/module/SetPolicy.cmake
@@ -1,7 +1,22 @@
 include_guard()
 
+include(LogVars)
+
 function(set_policy policy value)
 	if(POLICY ${policy})
+		cmake_policy(GET ${policy} before)
 		cmake_policy(SET ${policy} ${value})
+		cmake_policy(GET ${policy} after)
+	else()
+		set(before "NOTFOUND")
+		set(after "${before}")
 	endif()
+
+	if (before STREQUAL after)
+		set(${policy} "${before}")
+	else()
+		set(${policy} "${before} -> ${after}")
+	endif()
+
+	log_vars(${policy} MODE DEBUG)
 endfunction()
