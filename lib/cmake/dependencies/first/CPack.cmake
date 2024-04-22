9282239e778cf426eb0bb83e2c828832e5362bd9
dstodev <dstodev@gmail.com>
2024-04-21 19:17:38 -0700

    CLion cleanup

--- lib/cmake/dependencies/first/CPack.cmake
+++ lib/cmake/dependencies/first/CPack.cmake
@@ -1,12 +1,14 @@
 # This file is included first, before all other dependencies.
 # Ensures default CPack variables, except what is set here.
 
-message(DEBUG "Prior CPack configuration:")
-log_all_vars(MODE DEBUG FILTER CPACK)
+if(PROJECT_IS_TOP_LEVEL)
+	message(DEBUG "Prior CPack configuration:")
+	log_all_vars(MODE DEBUG FILTER CPACK)
 
-set(CPACK_GENERATOR "TXZ")
-set(CPACK_THREADS 0)  # https://cmake.org/cmake/help/latest/module/CPack.html#variable:CPACK_THREADS
-set(CPACK_ARCHIVE_THREADS 0)
-set(CPACK_VERBATIM_VARIABLES TRUE)
+	set(CPACK_GENERATOR "TXZ")
+	set(CPACK_THREADS 0)  # https://cmake.org/cmake/help/latest/module/CPack.html#variable:CPACK_THREADS
+	set(CPACK_ARCHIVE_THREADS 0)
+	set(CPACK_VERBATIM_VARIABLES TRUE)
 
-include(CPack)  # https://cmake.org/cmake/help/latest/module/CPack.html
+	include(CPack)  # https://cmake.org/cmake/help/latest/module/CPack.html
+endif()
