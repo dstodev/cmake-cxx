aee113f3fb34b8b0fcf460cf36b4073d8dacaea2
dstodev <dstodev@gmail.com>
2024-04-24 00:46:05 -0700

    RecordAliasDefinitions improvement

--- lib/cmake/module/RecordAliasDefinitions.cmake
+++ lib/cmake/module/RecordAliasDefinitions.cmake
@@ -2,7 +2,9 @@ include_guard()
 
 #[[
 	CMake does not provide a way to detect library aliases.
-	Instead, override add_library in the current scope to record alias definitions.
+	Instead, override add_library() to record alias definitions.
+
+	This is a heavy-handed approach--may only override add_library() once.
 
 	After calling this function, the following variables are available:
 	- ${out_names} - name of the variable that contains the list of alias names
@@ -13,39 +15,44 @@ include_guard()
 		# ...
 		message("Alias names: ${${ALIAS_NAMES}}")
 		message("Alias targets: ${${ALIAS_TARGETS}}")
+
+	Repeatedly calling this function updates output variables accordingly.
 ]]
-macro(record_alias_definitions out_names out_targets)
-	set(__${out_names} "" CACHE INTERNAL "")
-	set(__${out_targets} "" CACHE INTERNAL "")
+function(record_alias_definitions out_names out_targets)
+	set(__out_names_var "__${out_names}" CACHE INTERNAL "")
+	set(__out_targets_var "__${out_targets}" CACHE INTERNAL "")
+	set(${__out_names_var} "" CACHE INTERNAL "")
+	set(${__out_names_var} "" CACHE INTERNAL "")
+	set(${out_names} "${__out_names_var}" PARENT_SCOPE)
+	set(${out_targets} "${__out_targets_var}" PARENT_SCOPE)
+endfunction()
 
-	set(${out_names} "__${out_names}")
-	set(${out_targets} "__${out_targets}")
+function(add_library)
+	list(FIND ARGN ALIAS alias_index)
+	#                      alias_index
+	#          name_index       |       target_index
+	#             0|           1|            2|
+	# add_library(Alias::Name ALIAS some_target)
 
-	function(_record_alias alias_name alias_target)
-		message(VERBOSE "Detected library alias: ${alias_name} -> ${alias_target}")
-		set(alias_names ${__${out_names}})
-		set(alias_targets ${__${out_targets}})
-		list(APPEND alias_names ${alias_name})
-		list(APPEND alias_targets ${alias_target})
-		set(__${out_names} ${alias_names} CACHE INTERNAL "")
-		set(__${out_targets} ${alias_targets} CACHE INTERNAL "")
-	endfunction()
+	math(EXPR last_index "${ARGC} - 1")
 
-	function(add_library)
-		list(FIND ARGN ALIAS alias_index)
-		#                                               alias_index
-		#                                        name_index  |  target_index
-		#                                           0|      1|       2|
-		if(alias_index GREATER 0)  # add_library(My::Alias ALIAS some_target)
-			math(EXPR alias_name_index "${alias_index} - 1")
-			math(EXPR alias_target_index "${alias_index} + 1")
-			list(GET ARGN ${alias_name_index} alias_name)
-			list(GET ARGN ${alias_target_index} alias_target)
-			_record_alias(${alias_name} ${alias_target})
-		endif()
+	if(alias_index GREATER 0 AND alias_index LESS ${last_index})
+		math(EXPR name_index "${alias_index} - 1")
+		math(EXPR target_index "${alias_index} + 1")
+		list(GET ARGN ${name_index} alias_name)
+		list(GET ARGN ${target_index} alias_target)
+		_record_alias(${alias_name} ${alias_target})
+	endif()
 
-		# use function ARGN, not macro ARGN
-		set(_argn "ARGN")
-		_add_library(${${_argn}})
-	endfunction()
-endmacro()
+	_add_library(${ARGN})
+endfunction()
+
+function(_record_alias alias_name alias_target)
+	message(VERBOSE "Detected library alias: ${alias_name} -> ${alias_target}")
+	set(alias_names "${${__out_names_var}}")
+	set(alias_targets "${${__out_targets_var}}")
+	list(APPEND alias_names ${alias_name})
+	list(APPEND alias_targets ${alias_target})
+	set(${__out_names_var} "${alias_names}" CACHE INTERNAL "")
+	set(${__out_targets_var} "${alias_targets}" CACHE INTERNAL "")
+endfunction()
