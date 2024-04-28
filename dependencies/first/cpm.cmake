4b48421ebbbf023b66796781631ac02bf74f8f5a
dstodev <dstodev@gmail.com>
2024-04-27 20:02:54 -0700

    CPM.cmake update

--- dependencies/first/cpm.cmake
+++ dependencies/first/cpm.cmake
@@ -7,19 +7,20 @@ function(_configure)
 	set(output_root "${PROJECT_BINARY_DIR}/cmake")
 	set(output_path "${output_root}/CPM.cmake")
 
-	set(version "0.38.7")  # Versions: https://github.com/cpm-cmake/CPM.cmake/releases
-	set(url "https://github.com/TheLartians/CPM.cmake/releases/download/v${version}/CPM.cmake")
-	set(expected_hash "14ea07dfb484cad5db4ee1c75fd6a911")
+	set(version "0.39.0")  # Versions: https://github.com/cpm-cmake/CPM.cmake/releases
+	set(url "https://github.com/cpm-cmake/CPM.cmake/releases/download/v${version}/CPM.cmake")
+	set(expected_hash "04eefa38baf672f7e8fcd09075122517")
 
 	list(APPEND CMAKE_MODULE_PATH "${output_root}")
 	list(APPEND CMAKE_PREFIX_PATH "${output_root}")
 
 	if(NOT EXISTS "${output_path}")
 		message(STATUS "Downloading CPM.cmake to ${output_path}")
-		file(DOWNLOAD ${url} "${output_path}")
+		file(DOWNLOAD "${url}" "${output_path}")
 	endif()
 
 	file(MD5 "${output_path}" output_hash)
+
 	if(NOT "${output_hash}" STREQUAL "${expected_hash}")
 		line_number(hash_line "${CMAKE_CURRENT_FUNCTION_LIST_FILE}" ${expected_hash})
 		string(JOIN "\n " msg " "
