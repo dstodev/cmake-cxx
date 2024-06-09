c1277d78ad1a58369d0d05fee76ce7034e69dc0a
dstodev <dstodev@gmail.com>
2024-06-08 20:44:20 -0700

    Try linking to threads

--- lib/cmake/module/AddLocalAllTarget.cmake
+++ lib/cmake/module/AddLocalAllTarget.cmake
@@ -1,15 +1,15 @@
 include_guard()
 
-function(add_local_all_target target_name)
+function(add_local_all_target name)
 	get_directory_property(targets BUILDSYSTEM_TARGETS)
 
 	string(JOIN " " pretty_targets ${targets})
-	message(DEBUG "Adding target ${target_name} with: ${pretty_targets}")
+	message(DEBUG "Adding target ${name} with: ${pretty_targets}")
 
 	foreach(target IN LISTS targets)
 		list(APPEND argv "COMMAND;${target}")
 	endforeach()
 
-	add_custom_target(${target_name} ${argv} VERBATIM)
-	add_dependencies(${target_name} ${targets})
+	add_custom_target(${name} ${argv} VERBATIM)
+	add_dependencies(${name} ${targets})
 endfunction()
